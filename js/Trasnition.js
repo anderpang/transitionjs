40192a746df4994bb261acc76b9f738231c7ebaf
Ander <anderpang@qq.com>
2018-09-29 09:46:48 +0800

    Update Trasnition.js

--- js/Trasnition.js
+++ js/Trasnition.js
@@ -1,6 +1,6 @@
-/**
+/*!
  * Transition.js 1.0.0
- * https://github.com/anderpang/transition
+ * https://github.com/anderpang/transitionjs
  * <anderpang@foxmail.com>
  */
 "use strict";
@@ -8,6 +8,36 @@
  (function(scope,factory){
     typeof module !== "undefined" && module.exports?module.exports=factory():scope.Transition=factory();    
  })(this,function(){
+
+    if (typeof Object.assign !== 'function') {
+        // Must be writable: true, enumerable: false, configurable: true
+        Object.defineProperty(Object, "assign", {
+          value: function assign(target, varArgs) { // .length of function is 2
+            if (target == null) { // TypeError if undefined or null
+              throw new TypeError('Cannot convert undefined or null to object');
+            }
+      
+            var to = Object(target);
+      
+            for (var index = 1; index < arguments.length; index++) {
+              var nextSource = arguments[index];
+      
+              if (nextSource != null) { // Skip over if undefined or null
+                for (var nextKey in nextSource) {
+                  // Avoid bugs when hasOwnProperty is shadowed
+                  if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    to[nextKey] = nextSource[nextKey];
+                  }
+                }
+              }
+            }
+            return to;
+          },
+          writable: true,
+          configurable: true
+        });
+      }
+
      //当前播放列表
      var playList=Object.create(null),
          pfm=performance,
@@ -605,4 +635,10 @@
 
     return Transition;
  });
+
+
+
+
+
+
 
